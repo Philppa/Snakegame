87e33f66ea2554e6b1dafe0744cdf6b8597ee5d8
Philppa <whitetigerjones@gmail.com>
2024-06-18 05:59:59 -0500

    Fixed problem where it wouldn't restart game after losing

--- SnakeWeb/SnakeScript.js
+++ SnakeWeb/SnakeScript.js
@@ -37,10 +37,17 @@ function startGame() {
 }
 
 function gameOver() {
+    score = 0;
+    snake = [
+        { x: 200, y: 200 },
+        { x: 190, y: 200 },
+        { x: 180, y: 200 },
+        { x: 170, y: 200 },
+        { x: 160, y: 200 }
+    ]
     document.getElementById("gameCanvas").style.display = 'none';
     document.getElementById("startGame").style.display = 'block';
-    score = 0;
-    // gameOver screen not restarting game 
+    
 }
 
 function main() {
